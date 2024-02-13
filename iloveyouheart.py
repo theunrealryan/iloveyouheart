d17a6b5ba8e7f3cce1dfb0a294540a3faf7cfc1d
Ryan Ricardo de Souza <140509330+theunrealryan@users.noreply.github.com>
2024-02-13 20:37:49 -0300

    Add files via upload

---
+++ iloveyouheart.py
@@ -0,0 +1,31 @@
+import turtle
+t = turtle.Turtle()
+turtle.title("para minha princesa")
+
+screen = turtle.Screen()
+screen.bgcolor("black")
+
+t.color("red")
+t.fillcolor("red")
+t.begin_fill()
+
+t.left(140)
+t.forward(180)
+t.circle(-90, 200)
+t.setheading(60)
+t.circle(-90, 200)
+t.forward(100)
+
+t.end_fill()
+
+t.up()
+t.setpos(-80, 150)
+t.down()
+t.color("white")
+t.write("I LOVE YOU", font=("Rockwell Nova", 20, "bold"))
+
+t.ht()
+
+turtle.mainloop
+
+screen.exitonclick()
